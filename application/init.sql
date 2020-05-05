8d0804a538ddb5af5ca3395435e503022b6cc90c
Eperjesi Attila Dávid <eper.atis@gmail.com>
2020-05-05 17:29:00 +0200

    Database initialize

--- application/init.sql
+++ application/init.sql
@@ -1,13 +1,19 @@
-create table cities(
+create table students(
     id int not null AUTO_INCREMENT,
-    postal_code char(4) not null,
-    name varchar(250) not null,
+    firstName varchar(250) not null,
+    lastName varchar(250) not null,
+    osztaly char(4) not null,
 
-    constraint UQ_cities_postal_code unique(postal_code),
-    constraint UQ_cities_name unique(name),
-    constraint PK_cities primary key(id)
+    constraint PK_students primary key(id)
     );
 
-alter table employees add COLUMN 
-    path varchar(250) not null after tin
-    ;
+create table teachers(
+    id int not null AUTO_INCREMENT,
+    firstName varchar(250) not null,
+    lastName varchar(250) not null,
+    email varchar(250) not null,
+    path varchar(250) not null,
+    osztaly char(4) not null,
+
+    constraint PK_students primary key(id)
+    );
