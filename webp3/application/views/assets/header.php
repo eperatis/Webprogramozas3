7af3ef4e45f05b1510dc61e019d28560801715f8
Eperjesi Attila Dávid <eper.atis@gmail.com>
2020-05-20 12:23:37 +0200

    update css for profile

--- webp3/application/views/assets/header.php
+++ webp3/application/views/assets/header.php
@@ -7,10 +7,12 @@
     </head>
     <body>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
-
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
               <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
+                <li class="nav-item active mt-2 mt-lg-0">
                   <a class="nav-link" href="#">Kezdőlap <span class="sr-only">(current)</span></a>
                 </li>
                 <li class="nav-item">
--- webp3/application/views/teachers/profile.php
+++ webp3/application/views/teachers/profile.php
@@ -1,22 +1,30 @@
 <?php $this->load->view('assets/header'); ?>
-
-<img src="<?php echo base_url();echo $data->{'path'}; ?>" />
-
-<p>
-    <strong>Név:</strong>
-    <?php print_r($data->{'lastName'}); ?>
-    <?php echo ' '; ?>
-    <?php print_r($data->{'firstName'}); ?>
-</p>
-<p>
-    <strong>Email:</strong>
-    <?php print_r($data->{'email'}); ?>
-</p>
-<p>
-    <strong>Tanított osztály:</strong>
-    <?php print_r($data->{'osztaly'}); ?>
-    <?php echo '. osztály'; ?>
-</p>
-
+<div class="d-flex justify-content-center">
+    <div class="text-center">
+        <div style="text-align: center;
+                background-color: white;
+                box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+                ">
+            <img style="width: 100%;
+                        height: auto;
+                        " src="<?php echo base_url();echo $data->{'path'}; ?>" />
+            <p>
+                <strong>Név:</strong>
+                <?php print_r($data->{'lastName'}); ?>
+                <?php echo ' '; ?>
+                <?php print_r($data->{'firstName'}); ?>
+            </p>
+            <p>
+                <strong>Email:</strong>
+                <?php print_r($data->{'email'}); ?>
+            </p>
+            <p>
+                <strong>Tanított osztály:</strong>
+                <?php print_r($data->{'osztaly'}); ?>
+                <?php echo '. osztály'; ?>
+            </p>
+        </div>
+    </div>
+</div>
 <?php $this->load->view('assets/footer'); ?>
 
